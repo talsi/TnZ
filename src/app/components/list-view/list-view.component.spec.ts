78be5be89c2405907df20742ce992bbb75bbfb61
talsi <talsi534534534>
2016-10-02 14:12:52 +0300

    refactor #2

--- src/app/components/list-view/list-view.component.spec.ts
+++ src/app/components/list-view/list-view.component.spec.ts
@@ -5,7 +5,7 @@ import { ListViewComponent } from './list-view.component';
 
 describe('Component: ListView', () => {
   it('should create an instance', () => {
-    let component = new ListViewComponent();
-    expect(component).toBeTruthy();
+    //let component = new ListViewComponent();
+    expect(false).toBeTruthy();
   });
 });
